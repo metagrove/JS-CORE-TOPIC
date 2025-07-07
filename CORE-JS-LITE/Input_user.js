a6fd320a4eb971116698325540fa0b8aecb99664
tarun <taruninfo.acc@gmail.com>
2025-07-07 22:53:46 +0530

    Add files via upload

---
+++ CORE-JS-LITE/Input_user.js
@@ -0,0 +1,5 @@
+var prompt = require('prompt-sync')()
+const input = prompt("Enter a : ");
+
+const b = 2 + (parseInt(input));
+console.log(`val is ${b}`);
---
+++ CORE-JS-LITE/arthimetic.js
@@ -0,0 +1,9 @@
+// arthimetic = + - * / %
+
+// -----type casting -----
+// type casting = converting one type to another type
+// Number()
+// parseInt()
+// parseFloat()
+// String()
+// toString()
