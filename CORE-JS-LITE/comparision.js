d0851148009bcb3729ad803dda43009ea768fa71
tarun <taruninfo.acc@gmail.com>
2025-07-08 23:48:48 +0530

    Add files via upload

---
+++ CORE-JS-LITE/comparision.js
@@ -0,0 +1,97 @@
+// == Logical Operators ==
+
+// value = true or false
+// typeof value = 
+
+// ==
+// ===
+// !=
+// !==
+// >
+// <
+// >=
+// <=
+// &&
+
+
+console.log(1===1) // true
+console.log(1===2) // false
+console.log(1==1) // true
+console.log(1==2) // false
+console.log(1!=1) // false
+console.log(1!=2) // true
+console.log(1!==1) // false
+console.log(1!==2) // true
+console.log(1>1) // false
+console.log(1>2) // false
+console.log(1<1) // false
+console.log(1<2) // true
+console.log(1>=1) // true
+console.log(1>=2) // false
+console.log(1<=1) // true
+console.log(1<=2) // true
+console.log(1 && 1) // true
+console.log(1 && 0) // false
+console.log(0 && 1) // false
+console.log(0 && 0) // false
+console.log(1 || 1) // true
+console.log(1 || 0) // true
+console.log(0 || 1) // true
+console.log(0 || 0) // false
+console.log(!1) // false
+console.log(!0) // true
+console.log(!"") // true
+console.log(!"hello") // false
+console.log(!null) // true
+console.log(!undefined) // true
+console.log(!NaN) // true
+console.log(!Infinity) // false
+console.log(!-1) // false
+console.log(!0.1) // false
+console.log(!1.1) // false
+console.log(!true) // false
+console.log(!false) // true
+console.log(![]) // true
+console.log(![1, 2, 3]) // false
+console.log(!{}) // false
+console.log(!{a: 1}) // false
+console.log(!function(){}) // false
+console.log(!function(){}()) // false
+console.log(!new Date()) // false
+console.log(!new RegExp()) // false
+console.log(!new Map()) // false    
+console.log(!new Set()) // false
+console.log(!new WeakMap()) // false
+console.log(!new WeakSet()) // false
+console.log(!new Array()) // false
+console.log(!new Object()) // false
+console.log(!new String()) // false
+console.log(!new Number()) // false
+console.log(!new Boolean()) // false
+console.log(!new Error()) // false
+console.log(!new TypeError()) // false
+console.log(!new ReferenceError()) // false
+console.log(!new SyntaxError()) // false
+console.log(!new EvalError()) // false
+console.log(!new RangeError()) // false
+console.log(!new URIError()) // false
+console.log(!new AggregateError()) // false
+console.log(!new InternalError()) // false
+console.log(!new SystemError()) // false
+console.log(!new AssertionError()) // false
+console.log(!new LookupError()) // false
+console.log(!new MemoryError()) // false
+console.log(!new NotImplementedError()) // false
+console.log(!new NotFoundError()) // false
+console.log(!new PermissionError()) // false
+console.log(!new TimeoutError()) // false
+console.log(!new OverflowError()) // false
+console.log(!new UnderflowError()) // false
+console.log(!new ZeroDivisionError()) // false
+console.log(!new StopIteration()) // false
+console.log(!new StopAsyncIteration()) // false
+console.log(!new GeneratorExit()) // false
+console.log(!new KeyboardInterrupt()) // false
+console.log(!new SystemExit()) // false
+console.log(!new GeneratorExit()) // false
+console.log(!new StopIteration()) // false
