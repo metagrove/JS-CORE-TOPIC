7ebbed1ee9d84a4813e626e844e79b9bb16fa065
tarun <taruninfo.acc@gmail.com>
2025-08-08 19:20:10 +0530

    Add files via upload

---
+++ CORE-JS-LITE/switch.js
@@ -0,0 +1,13 @@
+const data = 1;
+
+switch (data){
+    case 1:
+        console.log("hello")
+        break;
+    case 2:
+        console.log("helo")
+        break;
+    default:
+        console.log("default")
+        break; 
+}
